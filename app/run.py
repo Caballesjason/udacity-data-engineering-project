f92b9774a055b5655d3c4e72de402ef53d208144
Caballesjason <jasoncaballes99@gmail.com>
2025-02-22 12:58:10 -0500

    Updated import line for joblib due to different python versions

--- app/run.py
+++ app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib ## from sklearn.externals 
 from sqlalchemy import create_engine
 
 
